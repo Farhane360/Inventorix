fbd121a1d2bad2f085bbe2d124bb1b8112a13443
Farhane360 <mannfrie360@gmail.com>
2025-07-27 02:03:38 +0100

    Mise a jour du code

--- backend/inventory/urls.py
+++ backend/inventory/urls.py
@@ -4,6 +4,4 @@ from .views import ProductListCreateView, ProductRetrieveUpdateDestroyView
 urlpatterns = [
     path('products/', ProductListCreateView.as_view(), name='product-list-create'),
     path('products/<int:pk>/', ProductRetrieveUpdateDestroyView.as_view(), name='product-detail'),
-    path('products/', ProductListCreateView.as_view()),
-    path('products/<int:pk>/', ProductRetrieveUpdateDestroyView.as_view()),
 ]
